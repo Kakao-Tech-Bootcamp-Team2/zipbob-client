1e33d41c031be121c9a0b469b6ec1ad931626b13
MINSEO PARK <miinseo0324@gmail.com>
2024-12-17 23:48:12 +0900

    feat: 로그인 페이지 UI 구현
    - 로그인 페이지에 구글, 네이버, 카카오 로그인 버튼 추가
    - 각 버튼 아이콘 및 텍스트 정렬과 간격을 조정

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,30 @@
+import styles from "../styles/LoginPage.module.css";
+import logoImage from "../assets/images/logo.png"; // 집밥 로고
+import googleLogo from "../assets/images/googleLogo.png";
+import kakaoLogo from "../assets/images/kakaoLogo.png";
+import naverLogo from "../assets/images/naverLogo.png";
+
 const LoginPage = () => {
-    return <div>로그인 페이지입니다.</div>;
-  };
-  
-  export default LoginPage;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logoSection}>
+        <img src={logoImage} alt="ZipBob 로고" className={styles.logo} />
+      </div>
+
+      {/* 로그인 버튼들 */}
+      <div className={styles.buttonSection}>
+        <button className={`${styles.loginButton} ${styles.google}`}>
+          <img src={googleLogo} alt="구글 로고" className={styles.icon}/> 구글 로그인으로 시작하기
+        </button>
+        <button className={`${styles.loginButton} ${styles.naver}`}>
+          <img src={naverLogo} alt="네이버 로고" className={styles.icon}/> 네이버 로그인으로 시작하기
+        </button>
+        <button className={`${styles.loginButton} ${styles.kakao}`}>
+          <img src={kakaoLogo} alt="카카오 로고" className={styles.icon}/> 카카오 로그인으로 시작하기
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
