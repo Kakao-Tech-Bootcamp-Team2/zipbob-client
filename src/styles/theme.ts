dc83c319e7f534a8b66005370d7a54026f9fbe5b
sayyyho <323psh@naver.com>
2024-12-19 02:36:29 +0900

    init: pr, issue template 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,1 +1,32 @@
-// TODO - 디자인 시스템 넣기
+type Font = {
+  fontFamily: string;
+  fontSize: string;
+  fontStyle: string;
+  fontWeight: string;
+  lineHeight: string;
+};
+
+type Theme = {
+  fonts: {
+    [key: string]: Font;
+  };
+  colors: {
+    [key: string]: string;
+  };
+};
+
+const fontGenerator = (
+  fontFamily: string,
+  fontSize: string,
+  fontStyle: string,
+  fontWeight: string,
+  lineHeight: string
+): Font => ({ fontFamily, fontSize, fontStyle, fontWeight, lineHeight });
+export const theme: Theme = {
+  fonts: {
+    test: fontGenerator("test", "test", "test", "test", "test"),
+  },
+  colors: {
+    background: "#F6F8FF",
+  },
+};
