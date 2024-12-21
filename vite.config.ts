bed7e83e8fe72f4a656e6b932794a1f8fece3035
sayyyho <323psh@naver.com>
2024-12-21 15:59:20 +0900

    Chore: 로컬 환경 https 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
+import mkcert from "vite-plugin-mkcert";
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), mkcert()],
+  server: {
+    https: true,
+  },
+});
