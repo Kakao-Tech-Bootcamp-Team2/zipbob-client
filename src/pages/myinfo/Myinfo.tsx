dda5e810dab5e17e861d147bfcf1216c54660873
sayyyho <323psh@naver.com>
2024-12-24 18:47:29 +0900

    Test: 로그아웃 테스트

--- src/pages/myinfo/Myinfo.tsx
+++ src/pages/myinfo/Myinfo.tsx
@@ -3,11 +3,17 @@ import { Title } from "../../components/title/Title";
 
 import { useEffect } from "react";
 import { getUserInfo, patchLogout } from "../../apis/auth";
+import { useNavigate } from "react-router-dom";
 
 export const MyInfo = () => {
+  const navigate = useNavigate();
   useEffect(() => {
     getUserInfo();
   }, []);
+  const handleLogin = () => {
+    patchLogout(localStorage.getItem("nickname") || "");
+    navigate("/login");
+  };
   return (
     <S.Layout>
       <Title url="" title="내 정보" $top="5%" $imageTop="" $right="-30%" />
@@ -15,7 +21,7 @@ export const MyInfo = () => {
         <S.decofri src="/images/fri.svg" />
         <S.TextZone>{localStorage.getItem("nickname") || "test"}</S.TextZone>
         <S.TextZone>{localStorage.getItem("email") || "test"}</S.TextZone>
-        <S.ModifyButton onClick={() => patchLogout}>로그아웃</S.ModifyButton>
+        <S.ModifyButton onClick={() => handleLogin()}>로그아웃</S.ModifyButton>
       </S.InfoWrapper>
     </S.Layout>
   );
